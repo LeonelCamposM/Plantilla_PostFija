26a7be64d2e2f1ed6c00cd726df02d8f3a1ff335
LeonelCamposM <leonelscamposm@gmail.com>
2022-06-08 18:27:12 -0600

    add postfija calc ijiijiijj

--- Plantilla_PostFija/Plantilla_PostFija.cpp
+++ Plantilla_PostFija/Plantilla_PostFija.cpp
@@ -5,13 +5,60 @@
 #include "Dobles.h"
 #include "Conjuntos.h"
 #include "Operando.h"
+#include <stack>
 
 using namespace std;
 float Operar(float a, float b, char operando);
 bool EsOperando(char elemento);
+int esOperando(char elemento);
+double resolver(string expr);
+double operar(char operador, double a, double b);
+
+stack<double> pila;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	cout << resolver("58*6+") << endl;
 	return 0;
+}
+
+double resolver(string expr) {
+	for (int i = 0; i < expr.size(); i++) {
+		char elemento = expr[i];
+		if(esOperando(elemento)) {
+			pila.push(elemento - '0');
+		}else {
+			double b = pila.top();
+			pila.pop();
+			double a = pila.top();
+			pila.pop();
+			double res = operar(elemento, a,b);
+			pila.push(res);
+		}
+	}
+	return pila.top();
+}
+
+double operar(char operador, double a, double b) {
+	if (operador == '*') {
+		return a*b;
+	}
+	if (operador == '+') {
+		return a + b;
+	}
+	if (operador == '-') {
+		return a - b;
+	}
+	if (operador == '/') {
+		return a / b;
+	}
+}
+
+int esOperando(char elemento) {
+	if (elemento >= '0' && elemento <= '9') {
+		return 1;
+	}
+	else {
+		return 0;
+	}
 }
